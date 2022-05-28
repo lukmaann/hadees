2526fb22e143be1e4f1ebebe5d4bce3289cef1ac
LNUAKX <lukmaannadaf@gmail.com>
2022-05-28 12:03:04 +0530

    hadees

---
+++ index.js
@@ -0,0 +1,7 @@
+var hadees={
+    0:"Deen asaan hai",
+    1:"Jannat ki chabi namaz hai",
+    2:"Jo dhoka de oo hum musalmano me se nahi",
+    3:"Baat karne se pehle salam karo",
+    4:"hamesha sach baat karo"
+}
